e5e7640d3c75333d661feffd058cc117634db853
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-05-07 22:10:33 +0600

    Second problem is done

--- problem.ts
+++ problem.ts
@@ -6,3 +6,10 @@ const formatString: FormatString = (input: string, toUpper?: boolean) => {
   }
   return input.toUpperCase();
 };
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  let filtered = items.filter((items) => items.rating >= 4);
+  return filtered;
+}
